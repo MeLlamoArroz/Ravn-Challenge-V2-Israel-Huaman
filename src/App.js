2014085ad2032d0f4574d4e78c3410e37ed69e0e
MeLlamoArroz <mellamoarroz@gmail.com>
2020-10-25 20:29:18 -0500

    GraphQL connection with SWAPI-GRAPHQL established

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import LeftMenuBar from './components/left-bar-menu/leftMenuBar'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        Ravn Star Wars Registry
+      </div>
+      <LeftMenuBar/>
+      <div className="App-left-menu-bar">
+      </div>
+      <div className="App-main-page">
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,10 +4,35 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { ApolloProvider } from 'react-apollo';
+// import { ApolloClient } from 'apollo-client';
+import ApolloClient from 'apollo-boost';
+import { gql } from 'apollo-boost';
+
+const client = new ApolloClient({
+  uri: "https://swapi-graphql.netlify.app/.netlify/functions/index"
+});
+
+client.query({
+  query: gql`
+    {
+      allFilms {
+        films {
+          title
+          episodeID
+          director
+        }
+      }
+    }
+  `
+}).then(res => console.log(res.data))
+
 ReactDOM.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
+  // </React.StrictMode>,
   document.getElementById('root')
 );
 
